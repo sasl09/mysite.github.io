374afd566cbe9235e3bf21c3aa83e1dbeef4f9b7
Armandus <sasl09@outlook.com>
2020-04-03 15:01:37 -0600

    Updates

---
+++ precache-manifest.6c0c34421cd785a5bdb2b576926e754f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e6c05379aaa166b66411c6023843355",
+    "url": "/mysite.github.io/index.html"
+  },
+  {
+    "revision": "16f43c60996dc6a3de4d",
+    "url": "/mysite.github.io/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "f33565bfaa44283f032e",
+    "url": "/mysite.github.io/static/css/main.90647fd3.chunk.css"
+  },
+  {
+    "revision": "16f43c60996dc6a3de4d",
+    "url": "/mysite.github.io/static/js/2.986374fc.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/mysite.github.io/static/js/2.986374fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33565bfaa44283f032e",
+    "url": "/mysite.github.io/static/js/main.d6f5b90c.chunk.js"
+  },
+  {
+    "revision": "3c662edfe06ed8aafe34",
+    "url": "/mysite.github.io/static/js/runtime-main.e0d179aa.js"
+  },
+  {
+    "revision": "b62d965f3e7e4a7d1a0954522598dfd5",
+    "url": "/mysite.github.io/static/media/MePicPortfolio.b62d965f.jfif"
+  },
+  {
+    "revision": "c1a911da0ef2e625cdf0bfdc01554653",
+    "url": "/mysite.github.io/static/media/myAvatar.c1a911da.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/mysite/precache-manifest.5b9d6d58edb95880d6eff3c0f3b2f8dd.js"
+  "/mysite.github.io/precache-manifest.6c0c34421cd785a5bdb2b576926e754f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mysite/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mysite.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
