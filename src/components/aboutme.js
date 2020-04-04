009fa64b0c0f724a9097325a59f024df6a49e3c8
Armandus <sasl09@outlook.com>
2020-04-04 09:05:40 -0600

    Fixed responsiveness for Smartphones on about me section

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -9,7 +9,7 @@ class AboutMe extends Component {
       <div className="container-video">
         <h1 className="title-video">Here's a video about me</h1>
         <div className="video">
-          <ReactPlayer width="800px" height= "350px" url="https://www.youtube.com/watch?v=U6vhnTahBKA" controls />
+          <ReactPlayer width="800px" height= "300px" url="https://www.youtube.com/watch?v=U6vhnTahBKA" controls />
         </div>
       </div>
     )
